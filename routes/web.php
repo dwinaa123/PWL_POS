b62b57e2f07046ae7366ed049a66b6fbd34ed29e
dwinaa123 <dwina3186@gmail.com>
2024-03-05 17:22:45 +0700

    pratikum 6

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
-use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\KategoriController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +11,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index'])->name('level.index');
 Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
