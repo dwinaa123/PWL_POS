02aa6096626b9b830cd6b2756ec1db2757693133
dwinaa123 <dwina3186@gmail.com>
2024-03-06 14:33:09 +0700

    pratikum2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,22 +9,29 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
-        $data =[
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
+        //$data =[
+           // 'level_id' => 2,
+           // 'username' => 'manager_tiga',
+           // 'nama' => 'Manager 3',
+           // 'password' => Hash::make('12345')
             //'username' => 'customer-1',
            //'nama' => 'pelanggan pertama',
             //'password' => Hash::make('12345'),
             //'level_id' => 4
-        ];
+       // ];
         //UserModel::insert($data);
         //UserModel::where('username', 'customer-1')->update($data);
-        UserModel::create($data);
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        //UserModel::create($data);
+        //$user = UserModel::all();
+       // return view('user', ['data' => $user]);
+       $user =UserModel::findOr(20, ['username', 'nama'], function (){
+        abort(404);
+       });
+        
+       
+       return view('user', ['data' => $user]);
     }
 }
