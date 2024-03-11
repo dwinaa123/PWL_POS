dc6f86a2056f3f67069b603ece3bc9dce577263a
dwinaa123 <dwina3186@gmail.com>
2024-03-11 23:23:40 +0700

    pratikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
-
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
@@ -27,8 +25,22 @@ class UserController extends Controller
         //UserModel::create($data);
         //$user = UserModel::all();
        // return view('user', ['data' => $user]);
-       $user =UserModel::where('level_id', 2)->count();
+       //$user =UserModel::where('level_id', 2)->count();
        //dd($user);
+       //$user = UserModel::firstOrCreate([
+    //'username' => 'manager22',
+    //'nama' => 'Manager Dua Dua',
+    //'password' => Hash::make('12345'),
+   // 'level_id' => 2
+   $user = UserModel::firstOrNew(
+[
+    'username' => 'manager33',
+    'nama' => 'Manager Tiga Tiga',
+    'password' => Hash::make('12345'),
+    'level_id' => 2
+]
+);
+$user->save();
        return view('user', ['data' => $user]);
     }
 }
